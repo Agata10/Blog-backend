fd5897cbdcd0bc5418e20af1f47fe1606bc29e5e
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-24 14:48:44 -0600

    Add geting all posts by user with the specific id

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,13 +2,30 @@ const express = require("express");
 const router = express.Router();
 
 const users = require("../data/users");
+const posts = require("../data/posts");
 
 router.route("/").get((req, res) => {
   res.render("users", { title: "users", users });
 });
 
-router.route("/:id/posts/create").get((req, res) => {
-  res.send(req.params);
+//Retrieves all posts by a user with the specified id
+//it might have query to sort by id either desc or asc
+router.route("/:id/posts/").get((req, res, next) => {
+  const userPosts = posts.filter((p) => p.userId == req.params.id);
+  if (req.query.sortBy === "id:desc") {
+    userPosts.sort((a, b) => {
+      return b.id - a.id;
+    });
+  } else if (req.query.sortBy === "id:asc") {
+    userPosts.sort((a, b) => {
+      return a.id - b.id;
+    });
+  }
+  if (userPosts) {
+    return res.render("users", { title: "posts", users, userPosts });
+  } else {
+    next();
+  }
 });
 
 module.exports = router;
