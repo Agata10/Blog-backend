ee68dc427ccb141af28972024a33354dc504116b
Agata Deter <124964478+Agata10@users.noreply.github.com>
2024-04-24 11:57:39 -0600

    Create data to work with

---
+++ data/users.js
@@ -0,0 +1,22 @@
+const users = [
+  {
+    id: 1,
+    name: "Hermione",
+    username: "HermioneG123",
+    email: "hermione123@example.com",
+  },
+  {
+    id: 2,
+    name: "Harry",
+    username: "HarryP",
+    email: "Harry_P@example.com",
+  },
+  {
+    id: 3,
+    name: "Ronald",
+    username: "Weasley",
+    email: "Weasley@example.com",
+  },
+];
+
+module.exports = users;
